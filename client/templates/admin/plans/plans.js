1a8fe623cb28b9eef513c692f1f9df1768b2eeb2
martin byrne <marb61a@gmail.com>
2016-03-13 18:32:40 +0000

    added adplan events to plans.js

---
+++ client/templates/admin/plans/plans.js
@@ -0,0 +1,32 @@
+Template.editplan.helpers({
+    'checkValue' : function(val1, val2){
+        if(val1 == val2){
+			return "selected";
+		}
+    }
+});
+
+Template.addplan.events({
+    'submit .add-plan-form' : function(){
+        var plan_name = event.target.plan_name.value;
+        var plan_label = event.target.plan_label.value;
+        var days = event.target.days.value;
+        var description = event.target.description.value;
+        var is_default = event.target.is_default.value;
+        var price = event.target.price.value;
+        
+        Plans.insert{
+            plan_name: plan_name,
+            plan_label: plan_label,
+            days: days,
+            price: price,
+            description: description,
+            is_default: is_default
+        }
+        
+        toastr.success('Plan Added');
+        Router.go('/admin/plans');
+        
+        return false;
+    }
+})
