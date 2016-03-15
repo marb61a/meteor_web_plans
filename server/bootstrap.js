0a05fbd12cd43ea62a958ab40cdfe71704572d43
martin byrne <marb61a@gmail.com>
2016-03-15 14:18:43 +0000

    added create user to bootstrap

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -25,3 +25,29 @@ Meteor.startup(function(){
         });    
     }
 });
+
+Accounts.onCreateUser(function(options, user){
+	Subscibers.insert({
+		user_id: user._id,
+        user_email: user.emails[0].address,
+        plan_id: getDefaultPlan()._id,
+        plan_name: getDefaultPlan().plan_name,
+        plan_label: getDefaultPlan().plan_label,
+        plan_days: getDefaultPlan().days,
+        plan_description: getDefaultPlan().description,
+        plan_price: getDefaultPlan().price,
+        join_date: new Date()
+	});
+	
+	if(options.profile){
+		user.profile = options.profile;
+	}
+	
+	return user;
+});
+
+function getDefaultPlan(){
+	return Plans.findOne({
+		is_default: '1'
+	});	
+}
