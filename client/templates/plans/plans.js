ff2859daa7d8261fbfe69511f068f6cbf0b5dd7b
martin byrne <marb61a@gmail.com>
2016-03-16 11:05:41 +0000

    added plans.js

---
+++ client/templates/plans/plans.js
@@ -0,0 +1,21 @@
+Template.plans.events({
+    '.click .buy-plan' : function(){
+        var plan_id = event.currentTarget.id;
+		var plan_name = event.currentTarget.rel;
+
+		var plan_info = Plans.findOne({_id: plan_id});
+
+		Subscribers.insert({
+			plan_name: plan_info.plan_name,
+            plan_label: plan_info.plan_label,
+            plan_days: plan_info.days,
+            plan_price: plan_info.price,
+            plan_description: plan_info.description,
+            user_id: Meteor.userId(),
+            user_email: Meteor.user().emails[0].address,
+            join_date: new Date()
+		});
+
+		toastr.success('Joined Plan!');
+    }
+});
